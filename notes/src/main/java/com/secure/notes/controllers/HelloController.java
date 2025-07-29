fc5eabb4ce5ab5b7060a93a6f64f4a884793f614
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-29 18:06:27 +0700

    first commit

---
+++ notes/src/main/java/com/secure/notes/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package com.secure.notes.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "Hi";
+    }
+    
+}
